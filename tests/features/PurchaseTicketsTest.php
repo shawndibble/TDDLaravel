daebae7f404b045989a6b7e501cabc83df155d7e
Shawn <shawndibble@gmail.com>
2017-12-04 19:16:25 -0600

    initial setup + basic ticket/order.

---
+++ tests/features/PurchaseTicketsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentGateway;
+use App\Concert;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class PurchaseTicketsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function customer_can_purchase_concert_tickets()
+    {
+        $paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $paymentGateway);
+
+        $concert = factory(Concert::class)->create(['ticket_price'=>3250]);
+
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => $paymentGateway->getValidTestToken()
+        ]);
+
+        $this->assertResponseStatus(201);
+        $this->assertEquals(9750, $paymentGateway->totalCharges());
+
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNotNull($order);
+        $this->assertEquals(3, $order->tickets()->count());
+    }
+}
